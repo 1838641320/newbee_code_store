41b11b5356b4f6e4cbe0ebb4d8e8c06cde40ebea
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-01-30 17:02:45 +0800

    Add files via upload

---
+++ 20.1/实现二分查找ub，抄.cpp
@@ -0,0 +1,29 @@
+﻿#include<cstdio>
+#include<ctime>
+#include<cstdlib>
+#include<algorithm>
+constexpr int maxn=666;
+int a[maxn]={0},i;
+int b_search(int a[],int n,int key){
+	int lb=-1,ub=n,mid;
+	while(ub>1+lb){
+		mid=(lb+ub)/2;
+		if(a[mid]>=key) ub=mid;
+		else lb=mid;
+	}
+	return ub;
+}
+int bsearch2(int a[],int start,int end,int key){
+	int mid=(start+end)/2;
+	if(start+1>=end) return end;
+	if(a[mid]>=key) return bsearch2(a,start,mid,key);
+	return bsearch2(a,mid,end,key);
+}
+int main(){
+	int key;
+	srand(time(0));
+	for(i=0;i<maxn;i++) a[i]=rand();
+	std::sort(a,a+maxn);
+	//for(i=0;i<maxn;i++) printf("%d ",a[i]);
+	while(~scanf("%d",&key)) printf("%d %d\n",a[b_search(a,maxn,key)],a[bsearch2(a,0,maxn,key)]);
+}
---
+++ 20.1/尝试对hdu2086进行二分操作.cpp
@@ -0,0 +1,34 @@
+﻿#include<cstdio>
+#include<cstring>
+#include<algorithm>
+constexpr int maxn=3006;
+double a0,an1,c[maxn];
+int n,i;
+int cmp(double an){
+	double pre=a0,next;
+	for(i=1;i<=n;i++){
+		next=2*an+2*c[i]-pre;
+		pre=an;
+		an=next;
+	}
+	return next>=an1;
+}
+double b_search(){
+	double lb=-1e13,ub=1e13,mid;
+	while(ub>0.0001+lb){
+		mid=(lb+ub)/2;
+		if(cmp(mid)) ub=mid;
+		else lb=mid;
+	}
+	return (lb+ub)/2;
+}
+int main(){
+
+	while(~scanf("%d",&n)){
+		scanf("%lf%lf",&a0,&an1);
+		memset(c,0,3006*8);
+		for(i=1;i<=n;i++) scanf("%lf",&c[i]);
+		printf("%.2lf\n",b_search());
+	}
+}
+
