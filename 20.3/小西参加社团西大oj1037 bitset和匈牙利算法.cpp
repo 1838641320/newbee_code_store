fa8284054475db19572d6d407cc745afb61caf9f
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-03-19 11:40:33 +0800

    Update 小西参加社团西大oj1037 bitset和匈牙利算法.cpp

--- 20.3/小西参加社团西大oj1037 bitset和匈牙利算法.cpp
+++ 20.3/小西参加社团西大oj1037 bitset和匈牙利算法.cpp
@@ -1,35 +1,18 @@
-﻿#include<cstdio>
-#include<iostream>
-#include<algorithm>
-#include<deque>
-#include<bitset>
+#include<bits/stdc++.h>
 using namespace std;
 int n,m,vice[105],a,b;
 bitset<105> used;
 deque<int> g[105];
 int dfs(int x) {
-	for (int i:g[x]) {
+	for (int i:g[x]) 
 		if (!used.test(i)) {
 			used.flip(i);
-			if (vice[i]==0||dfs(vice[i])) {
-				vice[i]=x;
-				return 1;
-			}
+			if (vice[i]==0||dfs(vice[i])) return vice[i] = x;
 		}
-	}
 	return 0;
 }
 int main() {
-	cin>>n>>m;
-	while(cin>>a>>b){
-		g[a].push_back(b);
-		g[b].push_back(a);
-	}
-	for (int i = 1;i<=m;i++) {
-		used.reset();
-		dfs(i);
-	}
-	int ans=0;
-	for(int i=0;i<104;i++) if(vice[i]) ans++;
-	cout<<ans;
+	for(cin>>n>>m;cin>>a>>b;) g[a].push_back(b);
+	for (int i = 1;i<=m;i++) used.reset(),dfs(i);
+	cout<<count_if(vice,vice+104,[](int x){return x;});
 }
