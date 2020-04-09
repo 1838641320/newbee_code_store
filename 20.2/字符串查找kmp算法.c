9f35de26b46573a9bf0ca8ca5574accd1fcfdf04
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-04-09 18:21:21 +0800

    Add files via upload

--- 20.2/字符串查找kmp算法.c
+++ 20.2/字符串查找kmp算法.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 #include<string.h>
 #define maxn (int)1e5+8
-char pattern[maxn]= {0},text[maxn]= {0};
-int next[maxn]= {0};
+char pattern[maxn]={0},text[maxn]={0};
+int next[maxn]={0};
 void Next(char *p,int *next) {
 	next[0]=-1;
 	int len=strlen(p);
-	for(int i=1,j=0;i<len;) {
-		if(j==-1||p[i]==p[j]) next[++i]=++j;
+	for (int i=1,j=0;i<len;) {
+		if (j==-1||p[i]==p[j]) next[++i]=++j;
 		else j=next[j];
 	}
 }
-int KmpSearch(char* s, char* p) {
-	int i = 0,j = 0,sLen = strlen(s),pLen = strlen(p);
-	while (i < sLen && j < pLen) {
-		if (j == -1 || s[i] == p[j]) 
+int KmpSearch(char *s,char *p) {
+	int i=0,j=0,sLen=strlen(s),pLen=strlen(p);
+	while (i<sLen&&j<pLen) {
+		if (j==-1||s[i]==p[j])
 			i++,j++;
-		else j = next[j];
+		else j=next[j];
 	}
-	return j == pLen?i - j:-1;
+	return j==pLen?i-j:-1;
 }
 int main() {
 	scanf("%s%s",pattern,text);
