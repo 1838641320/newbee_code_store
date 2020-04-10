029aa564fd8a93671fd75b297f43f264bed2f6fa
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-04-10 18:08:29 +0800

    Add files via upload

---
+++ 20.3/Powerful array CodeForces - 86D 莫队.cpp
@@ -0,0 +1,35 @@
+﻿#include"stdafx.h"
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+const int maxn=2e5+9,maxx=1e6+9;
+int n,t,block;
+ll ans[maxn],cnt[maxx]={},a[maxn]={};
+struct qu { int x,y,i; } q[maxn]={};
+bool cmp(qu &a,qu &b) {
+	int ab=a.x/block,bb=b.x/block;
+	return ab^bb?ab<bb:(ab&1?a.y<b.y:a.y>b.y);
+}
+int main() {
+	cin>>n>>t;
+	block=ceil(sqrt(n));
+	for (int i=1;i<=n;i++) scanf("%lld",a+i);
+	for (int i=0;i<t;i++) scanf("%d %d",&q[i].x,&q[i].y),q[i].i=i;
+	sort(q,q+t,cmp);
+	ll sum=a[1];
+	cnt[a[1]]++;
+	for (int i=0,s=1,e=1;i<t;i++) {
+		int l=q[i].x,r=q[i].y;
+		while (s>l) 
+			sum+=(2ll*(cnt[a[--s]]++)+1ll)*a[s];
+		while (s<l) 
+			sum-=(2ll*(--cnt[a[s]])+1ll)*a[s++];
+		while (e>r) 
+			sum-=(2ll*(--cnt[a[e]])+1ll)*a[e--];
+		while (e<r) 
+			sum+=(2ll*(cnt[a[++e]]++)+1ll)*a[e];
+		ans[q[i].i]=sum;
+	}
+	for (int i=0;i<t;i++) cout<<ans[i]<<'\n';
+}
