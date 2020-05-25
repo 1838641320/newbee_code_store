97d09dac5352668a10e80e962f1b904eb5ea82e6
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-05-25 18:05:34 +0800

    Add files via upload

---
+++ 20.5/第十五届中北大学算法与程序设计竞赛 C 港口 区间修改和差分.cpp
@@ -0,0 +1,16 @@
+﻿#include"p.h"
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+using namespace std;
+const int maxn=100000+9;
+typedef long long ll;
+ll a[maxn]={},ps[maxn]={},n,up=0,dw=0;
+int main() {
+	cin>>n;
+	rep(i,0,n) scanf("%lld",a+i);
+	adjacent_difference(a,a+n,ps);
+	rep(i,1,n)
+		if (ps[i]>0) up+=ps[i];
+		else dw-=ps[i];
+	printf("%lld",max(up,dw));
+}
