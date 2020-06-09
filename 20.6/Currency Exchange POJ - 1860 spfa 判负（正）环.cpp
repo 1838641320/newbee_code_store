236247baa01fa1a798c1fe68f13870fac56dc997
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-06-09 23:12:27 +0800

    Add files via upload

---
+++ 20.6/Currency Exchange POJ - 1860 spfa 判负（正）环.cpp
@@ -0,0 +1,48 @@
+﻿//#include"s.h"
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<vector>
+using namespace std;
+struct P{
+	int t;
+	double r,c;
+	P(int x,double r,double c):t(x),r(r),c(c){}
+};
+const int maxn=109;
+const double ep=1e-10;
+vector<P> G[maxn];
+double dist[maxn]={};
+int n,m,x,u,v;
+double V,ruv,cuv,rvu,cvu,res;
+int spfa(){
+	int cnt[maxn]={};
+	bool iq[maxn]={};
+	dist[x]=V;
+	queue<int> q;
+	q.push(x);iq[x]=1;
+	while(!q.empty()){
+		int v=q.front();q.pop();
+		iq[v]=0;
+		for(int i=0;i<G[v].size();i++){
+			int t=G[v][i].t;
+			double cc=(dist[v]-G[v][i].c)*G[v][i].r;
+			if(dist[t]>=cc) continue;
+			dist[t]=cc;
+			if(iq[t]==0){
+				iq[t]=1;q.push(t);cnt[t]++;
+				if(cnt[t]>n) return 1;
+			}
+		}
+	}
+	return 0;
+}
+int main(){
+	scanf("%d%d%d%lf",&n,&m,&x,&V);
+	while(m--){
+		scanf("%d%d%lf%lf%lf%lf",&u,&v,&ruv,&cuv,&rvu,&cvu);
+		G[u].push_back(P(v,ruv,cuv));
+		G[v].push_back(P(u,rvu,cvu));
+	}
+	puts(spfa()?"YES":"NO");
+}
---
+++ 20.6/Wormholes POJ - 3259 bellman_ford 判负环.cpp
@@ -0,0 +1,50 @@
+﻿//#include"s.h"
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<cstring>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define mem(a,b) memset(a,b,sizeof a)
+using namespace std;
+const int maxn=500+9;
+typedef long long ll;
+int n,m,w;
+struct Edge{ 
+	int f,t,w;
+	Edge(int a,int b,int c):f(a),t(b),w(c){}
+};
+vector<Edge> G;
+int bellman_ford(){
+	int dist[maxn];
+	mem(dist,0x3f);dist[1]=0;
+	rep(i,0,n){
+		int f=0;
+		rep(j,0,G.size()){
+			if(dist[G[j].t]>dist[G[j].f]+G[j].w)
+				dist[G[j].t]=dist[G[j].f]+G[j].w,
+				f=1;
+		}
+		if(!f) return 0;
+	}
+	return 1;
+}
+int main(){
+	int f;cin>>f;
+	while(f--) {
+		int s,e,t;
+		cin>>n>>m>>w;
+		G.clear();
+		rep(i,0,m){
+			scanf("%d%d%d",&s,&e,&t);
+			G.push_back(Edge(s,e,t));
+			G.push_back(Edge(e,s,t));
+		}
+		rep(i,0,w){
+			scanf("%d%d%d",&s,&e,&t);
+			G.push_back(Edge(s,e,-t));
+		}
+		puts(bellman_ford()?"YES":"NO");
+	}
+}
