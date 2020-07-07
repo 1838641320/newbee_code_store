e8aad0126d9f012e5a3034e77ac19d21bdaf0c3a
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-07-07 17:41:05 +0800

    Add files via upload

---
+++ 20.7/Organising the Organisation UVA - 10766 生成树计数-Kirchhoff矩阵 高斯消元法算行列式 long double.cpp
@@ -0,0 +1,40 @@
+﻿#include"s.h"
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define mem(a,b) memset(a,b,sizeof a)
+using namespace std;
+typedef long double LD;
+const LD ep=1e-10;
+const int maxn=59;
+LD K[maxn][maxn];//Kirchhoff
+bool A[maxn][maxn];
+LD det(int n){
+	LD res=1;
+	rep(i,0,n){
+		int p=i;
+		rep(j,i+1,n)
+			if(fabs(K[j][i])>fabs(K[p][i])) p=j;
+		if(fabs(K[p][i])<ep) return 0;
+		swap(K[p],K[i]);
+		if(p!=i) res=-res;
+		res*=K[i][i];
+		rep(j,i+1,n) K[i][j]/=K[i][i];
+		rep(j,0,n) if(j!=i)
+			rep(k,i+1,n) K[j][k]-=K[j][i]*K[i][k];
+	}
+	return res;
+}
+int main(){
+	int n,m,k,x,y;
+	while(~scanf("%d%d%d",&n,&m,&k)){
+		mem(A,0),mem(K,0);
+		while(m--){
+			scanf("%d%d",&x,&y);
+			x--,y--;
+			A[x][y]=A[y][x]=1;
+		}
+		rep(i,0,n) rep(j,0,n) if(i!=j&&A[i][j]==0)
+			K[i][i]+=1,K[i][j]-=1;
+		printf("%.Lf\n",fabs(det(n-1)));
+	}
+}
---
+++ 20.7/失败算π2.cpp
@@ -0,0 +1,20 @@
+﻿#include"s.h"
+#include<cstdio>
+typedef long double ld;
+const ld one=1,two=2,three=3;
+int main(){
+	long double t=0L,sqrt3,lb=1.5L,ub=two,mid,q=0L;
+	printf("%d\n\n",sizeof(long double));
+	for(;q<900.0L;q+=one){
+		mid=(lb+ub)/two;
+		if(mid*mid<three){ lb=mid;continue; }
+		if(mid*mid>three){ ub=mid;continue; }
+	}
+	sqrt3=one/mid;
+	q=1;
+	for(ld i=sqrt3,add=-1.0L/three;q<100.0L;q+=two){
+		t+=i;
+		printf("%.40LF\n",t*6.0L);
+		i=i*add*q/(q+two);
+	}
+}
