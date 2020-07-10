f737f7980a01f0e2480a1fbdfc07d5ae0d7e3e2f
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-07-10 16:50:17 +0800

    Add files via upload

---
+++ 20.7/The Accomodation of Students HDU - 2444 染色法判二分图 二分图最大匹配.cpp
@@ -0,0 +1,50 @@
+﻿#include"s.h"
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define mem(a,b) memset(a,b,sizeof a)
+using namespace std;
+const int maxn=209;
+char used[maxn];
+int n,m,link[maxn],color[maxn];
+vector<int> g[maxn];
+bool dfs(int x){
+	for(int t:g[x]){
+		if(used[t]) continue;used[t]=1;
+		if(link[t]==-1||dfs(link[t]))
+			return link[t]=x,1;
+	}
+	return 0;
+}
+bool col(int x,int c){
+	color[x]=c;
+	for(int t:g[x]){
+		if(color[t]==c) return 0;
+		if(color[t]==0&&col(t,-c)==0) return 0;
+	}
+	return 1;
+}
+int main(){
+	int f,t;
+	while(~scanf("%d%d",&n,&m)){
+		mem(color,0);mem(link,-1);
+		rep(i,0,n+2) g[i].clear();
+		rep(i,0,m) {
+			scanf("%d%d",&f,&t);
+			g[f].push_back(t);
+			g[t].push_back(f);
+		}
+		int flag=1;
+		for(int i=1;i<=n;i++){
+			if(color[i]) continue;
+			if(col(i,1)==0) { flag=0; break; }
+		}
+		if(flag==0) { puts("No");continue; }
+		int ans=0;
+		for(int i=1;i<=n;i++){
+			if(color[i]==1){
+				mem(used,0),ans+=dfs(i);
+			}
+		}
+		printf("%d\n",ans);
+	}
+}
---
+++ 20.7/矩阵行变换.cpp
@@ -0,0 +1,143 @@
+﻿#include"s.h"
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define mem(a,b) memset(a,b,sizeof a)
+using namespace std;
+class gauss_cal{
+public:
+	struct fract{ int num,den; };
+	typedef vector<fract> vec;
+	typedef vector<vec> Matrix;
+	int line=3,col=3,precision=2;
+	Matrix matrix;
+	fract det={1,1};
+	void init(int l,int c){
+		matrix.clear();
+		line=l,col=c;
+		matrix.resize(l,vec(c));
+		rep(i,0,l) rep(j,0,c)
+			cin>>matrix[i][j].num,
+			matrix[i][j].den=1;
+	}
+	void print(){
+		for(auto &i:matrix) {
+			for(auto &j:i) {
+				printf("%d",j.num);
+				if(j.den!=1) printf("/%d ",j.den);
+				putchar('\t');
+			}
+			puts("\n");
+		}
+		puts("");
+	}
+	void Swap(int l1,int l2){
+		l1--,l2--;
+		swap(matrix[l1],matrix[l2]);
+	}
+	void add(int l1,int l2,int mul,int div){
+		if(l1<0||l2<0||l1>line||l2>line) return;
+		l1--,l2--;
+		for(int i=0;i<col;i++){
+			auto a=matrix[l1][i],b=matrix[l2][i];
+			a.num=a.num*b.den*div+b.num*a.den*mul;
+			a.den*=b.den*div;
+			matrix[l1][i]=simplify(a);
+		}
+	}
+	void mul(int l1,int mul,int div){
+		if(l1<0||l1>line) return;
+		l1--;
+		det=simplify({det.num*div,det.den*mul});
+		for(int i=0;i<col;i++){
+			auto a=matrix[l1][i];
+			a.num*=mul;
+			a.den*=div;
+			matrix[l1][i]=simplify(a);
+		}
+	}
+	fract simplify(fract a){
+		int g=gcd(a.num,a.den);
+		a.num/=g;
+		a.den/=g;
+		if(a.den<0) a.num*=-1,a.den*=-1;
+		return a;
+	}
+};
+void autosolve(gauss_cal::Matrix mat,gauss_cal::fract det){
+	int l=mat.size(),c=mat[0].size();
+	
+}
+int main(){
+	int l,c,l1,l2,pre=2;
+	char cmd[99],*p;
+	gauss_cal mat;
+	while(cin>>l>>c){
+		mat.init(l,c);
+		while(fgets(cmd,99,stdin),*cmd!='!'){
+			if(strcmp(cmd,"det\n")==0){
+				printf("%d",mat.det.num);
+				if(mat.det.den!=1) printf("/%d",mat.det.den);
+				puts("");
+				continue;
+			}
+			if(strcmp(cmd,"autosolve\n")==0){
+				autosolve(mat.matrix,mat.det);
+				continue;
+			}
+			p=cmd;
+			while(*p&&!isdigit(*p)) p++;
+			l1=strtol(p,&p,10);
+			if(count(cmd,cmd+99,' ')){
+				while(*p&&!isdigit(*p)) p++;
+				l2=strtol(p,&p,10);
+				mat.Swap(l1,l2);
+				mat.print();
+				continue;
+			}
+			int flag=count(cmd,cmd+99,'+')+count(cmd,cmd+99,'-')*2;
+			flag+=count(cmd,cmd+99,'*')*4+count(cmd,cmd+99,'/')*8;
+			int mul=flag&2?-1:1,div=1;
+			if(flag&4){
+				while(*p&&!isdigit(*p)) p++;
+				mul*=strtol(p,&p,10);
+			}
+			if(flag&8){
+				while(*p&&!isdigit(*p)) p++;
+				div=strtol(p,&p,10);
+			}
+			if(count(cmd,cmd+99,'r')==1)
+				mat.mul(l1,mul,div);
+			if(count(cmd,cmd+99,'r')==2){
+				while(*p&&!isdigit(*p)) p++;
+				l2=strtol(p,&p,10);
+				mat.add(l1,l2,mul,div);
+			}
+			mat.print();
+			memset(cmd,0,sizeof cmd);
+		}
+	}
+}
+/*
+
+one
+r1 r2 
+
+two
+r2+r2
+r1-r2
+r2+2*r1
+r2+3/5*r3
+
+three
+r1*4
+
+four
+det
+
+five
+!
+
+six
+autosolve
+
+*/
