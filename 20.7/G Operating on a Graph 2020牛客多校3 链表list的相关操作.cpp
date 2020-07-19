b45e0152c58b1c3a0b8d0036c514716a2dfb847a
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-07-19 18:10:45 +0800

    Add files via upload

---
+++ 20.7/G Operating on a Graph 2020牛客多校3 链表list的相关操作.cpp
@@ -0,0 +1,39 @@
+﻿#include"s.h"
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a,bb=b;i<bb;i++)
+using namespace std;
+typedef long long ll;
+const int maxn=8e5+9;
+int par[maxn]={};
+int findp(int x){ return par[x]==x?x:(par[x]=findp(par[x])); }
+list<int> g[maxn];
+void solve(){
+	int n,m;
+	scanf("%d%d",&n,&m);
+	rep(i,0,n) par[i]=i,g[i].clear();
+	rep(i,0,m){
+		int u,v;scanf("%d%d",&u,&v);
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	int q;scanf("%d",&q);
+	while(q--){
+		int o;scanf("%d",&o);
+		int po=findp(o);
+		if(po!=o) continue;
+		list<int> l;
+		for(int i:g[po]){
+			int pi=findp(i);
+			if(po==pi) continue;
+			l.splice(l.end(),g[pi]);
+			par[pi]=po;
+		}
+		l.swap(g[po]);
+	}
+	rep(i,0,n) printf("%d ",findp(i));
+	puts("");
+}
+int main(){
+	int t;scanf("%d",&t);
+	while(t--) solve();
+}
---
+++ 20.7/模拟退火 求多元函数极值.cpp
@@ -0,0 +1,46 @@
+﻿#include"s.h"
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+using namespace std;
+namespace SimulateAnneal{
+
+	mt19937 rd(time(0));
+	double precision=0.001,init_temp=1000,speed=0.97;
+	double (*cal)(vector<double> &a);
+	void init(double (*cal_)(vector<double> &a),double a=precision,double b=init_temp,double c=speed){
+		cal=cal_;
+		precision=a;
+		init_temp=b;
+		speed=c;
+	}
+	double rdd(){
+		return 1.0*rd()/rd.max();
+	}
+	vector<double> solve(vector<double> a){
+		vector<double> b,ans=a;
+		double ansv=cal(a);
+		for(double t=init_temp;t>precision;t*=speed){
+			b=a;
+			for(auto &i:b) i+=t*(rdd()*2.0-1.0);
+			double d=cal(b),expe=1.0*((ansv-d)/t*1.0);
+			if(expe>rdd())
+				a=b;
+			if(d<ansv) 
+				ans=b,ansv=d;
+		}
+		return ans;
+	}
+
+}
+double cal(vector<double> &a){
+	double sum=0;
+	double x=a[0],y=a[1];
+	sum=exp(3*x)*(x+y*y+2*y);
+	return sum;
+}
+int main(){
+	SimulateAnneal::init(cal,0.000001,100,0.98);
+	auto res=SimulateAnneal::solve({0,0});
+	for(auto i:res) printf("%lf ",i);
+	printf("\n%lf\n",cal(res));
+}
