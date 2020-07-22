a2ff240f49e167a2078b49fa65666db56a0a6fd6
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-07-22 09:46:26 +0800

    Add files via upload

---
+++ 20.7/2020 杭电多校1 Leading Robots 单调栈的应用.cpp
@@ -0,0 +1,36 @@
+﻿#include"s.h"
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn=5e4+9;
+typedef pair<int,int> pii;
+void solve(){
+	pair<int,int> a[maxn]={};
+	long double b[maxn]={};
+	set<pii> se;
+	int n;scanf("%d",&n);
+	for(int i=0;i<n;i++)
+		scanf("%d%d",&a[i].first,&a[i].second);
+	sort(a,a+n,greater<pii>());
+	for(int i=1;i<n;i++) if(a[i]==a[i-1])
+		se.insert(a[i]);
+	n=unique(a,a+n)-a;
+	vector<pii> ra={a[0]};
+	for(int i=1;i<n;i++) if(a[i].second>ra.back().second)
+		ra.push_back(a[i]);
+	vector<int> dq={0};
+	b[0]=0.0l;
+	for(int i=1;i<ra.size();i++){
+		while(dq.empty()==0){//用单调栈维护x轴坐标递增的点
+			int p=dq.back();
+			b[i]=(1.0l*ra[p].first-ra[i].first)/(1.0l*ra[i].second-ra[p].second);
+			if(b[i]>b[p]) break;
+			dq.pop_back();
+		}
+		dq.push_back(i);
+	}
+	printf("%d\n",count_if(dq.begin(),dq.end(),[&](int a){return se.count(ra[a])==0;}));
+}
+int main(){
+	int t;cin>>t;
+	while(t--) solve();
+}
---
+++ 20.7/辛普森积分.cpp
@@ -0,0 +1,21 @@
+﻿#include"s.h"
+#include<bits/stdc++.h>
+using namespace std;
+double eps=1e-6;
+double f(double x){
+	return 8*pow(sin(x),3);
+}
+double simpson(double l,double r){
+	return (f(l)+f(r)+4*f((l+r)/2))*(r-l)/6;
+}
+double cal(double l,double r,double S){
+	//printf("%lf %lf\n",l,r);
+	double L=simpson(l,(l+r)/2),R=simpson((l+r)/2,r);
+	if(fabs(L+R-S)<eps) return L+R;
+	return cal(l,(l+r)/2,L)+cal((l+r)/2,r,R);
+}
+int main(){
+	double l,r;
+	while(scanf("%lf %lf",&l,&r)==2)
+		printf("%lf\n",cal(l,r,simpson(l,r)));
+}
