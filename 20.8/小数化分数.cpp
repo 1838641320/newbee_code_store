a6293dfe093bab71d641e8b1e492a313d3bac747
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-08-14 20:16:22 +0800

    Update 小数化分数.cpp

--- 20.8/小数化分数.cpp
+++ 20.8/小数化分数.cpp
@@ -1,30 +1,39 @@
-#include<bits/stdc++.h>
-#define mem(a,b) memset(a,b,sizeof a)
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<cmath>
 using namespace std;
 typedef long long ll;
-template<typename T>
-void read(T&a){
-	a=0;int c=getchar(),f=0;
-	for(;!isdigit(c);c=getchar()) f|=c=='-';
-	for(;isdigit(c);c=getchar()) a=a*10+c-'0';
-	if(f) a=-a;
-}
-pair<ll,ll> find_f(double x,double eps=1e-10){
-	double delt=1e99;
-	ll lz=0,lm=1,rz=1,rm=1;
+pair<ll,ll> find_f(double x,double eps=1e-15){
+	double delt=1e99,t;
+	ll y=floor(x);
+	x=fmod(x,1);
+	ll lz=0,lm=1,rz=1,rm=1,midz,midm;
 	pair<ll,ll> res;
 	while(delt>eps){
-		ll midz=lz+rz,midm=lm+rm;
-		double t=1.0*midz/midm-x;
-		if(fabs(t)<delt) res={midz,midm},delt=fabs(t);
-		if(t>0) rz=midz,rm=midm;
-		else lz=midz,lm=midm;
+		midz=lz+rz,midm=lm+rm;
+		t=fabs(1.0*midz/midm-x);
+		if(delt>t) res={midz,midm},delt=t;
+		t=fabs(1.0*lz/lm-x);
+		if(delt>t) res={lz,lm},delt=t;
+		t=fabs(1.0*rz/rm-x);
+		if(delt>t) res={rz,rm},delt=t;
+		if(1.0*midz/midm>x) for(ll times=1;1.0*midz/midm>x;times*=2){
+			rz=midz,rm=midm;
+			midz+=times*lz;
+			midm+=times*lm;
+		}
+		else for(ll times=1;1.0*midz/midm<x;times*=2){
+			lz=midz,lm=midm;
+			midz+=times*rz;
+			midm+=times*rm;
+		}
 	}
+	res.first+=y*res.second;
 	return res;
 }
 int main(){
-	double x,eps=1e-10;
-	while(scanf("%lf%lf",&x,&eps)>0){
+	double x,eps=1e-15;
+	while(scanf("%lf",&x)>0){
 		auto res=find_f(x,eps);
 		printf("%lld/%lld\n",res.first,res.second);
 	}
