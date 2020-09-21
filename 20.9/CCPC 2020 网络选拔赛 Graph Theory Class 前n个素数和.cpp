d7a7ee7aaeb48e38625843d5e5ecbe35dd20a1a1
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-09-21 21:46:55 +0800

    Add files via upload

---
+++ 20.9/CCPC 2020 网络选拔赛 Graph Theory Class 前n个素数和.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define mem(a,b) memset(a,b,sizeof a)
+using namespace std;
+using ll=long long;
+const int maxn=2e5+9;//sqrt(n)*2
+ll s[maxn],v[maxn];
+ll ppt(ll a,ll b,ll c,ll d) { return a>=c?(b/a-1):(d-a); }
+ll preprimesum(ll n) {
+    ll r=sqrt(n),ndr=n/r,nv=r+ndr-1;
+    rep(i,0,r) v[i]=n/(i+1);
+    rep(i,r,nv) v[i]=v[i-1]-1;
+    rep(i,0,nv) s[i]=v[i]*(v[i]+1)/2-1;
+    for(ll p=2; p<=r; p++) {
+        if(s[nv-p]>s[nv-p+1]) {
+            ll sp=s[nv-p+1],p2=p*p;
+            rep(i,0,nv) {
+                if(v[i]>=p2)
+                    s[i]-=p*(s[ppt(v[i]/p,n,ndr,nv)]-sp);
+                else break;
+            }
+        }
+    }
+    return s[0];
+}
+void solve(){
+    ll n,k;scanf("%lld%lld",&n,&k);
+    __int128_t one=1,ans=one*(n+4)*(n-1);
+    ans=(ans/2)%k;
+    ans+=preprimesum(n+1);
+    ll ans1=(ans-2+k)%k;
+    cout<<ans1<<endl;
+}
+int main() {
+    int t;scanf("%lld",&t);
+    while(t--) solve();
+}
