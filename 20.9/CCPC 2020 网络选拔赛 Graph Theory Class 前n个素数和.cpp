e1ff95766fcaec7dcfb859bdf9bd11b21720d5df
1838641320 <60167568+1838641320@users.noreply.github.com>
2020-09-22 10:29:03 +0800

    Update CCPC 2020 网络选拔赛 Graph Theory Class 前n个素数和.cpp

--- 20.9/CCPC 2020 网络选拔赛 Graph Theory Class 前n个素数和.cpp
+++ 20.9/CCPC 2020 网络选拔赛 Graph Theory Class 前n个素数和.cpp
@@ -7,13 +7,13 @@ const int maxn=2e5+9;//sqrt(n)*2
 ll s[maxn],v[maxn];
 ll ppt(ll a,ll b,ll c,ll d) { return a>=c?(b/a-1):(d-a); }
 ll preprimesum(ll n) {
-	ll r=sqrt(n),ndr=n/r,nv=r+ndr-1;
-	rep(i,0,r) v[i]=n/(i+1);
-	rep(i,r,nv) v[i]=v[i-1]-1;
-	rep(i,0,nv) s[i]=v[i]*(v[i]+1)/2-1;
-	for(ll p=2; p<=r; p++) {
-		if(s[nv-p]>s[nv-p+1]) {
-			ll sp=s[nv-p+1],p2=p*p;
+    ll r=sqrt(n),ndr=n/r,nv=r+ndr-1;
+    rep(i,0,r) v[i]=n/(i+1);
+    rep(i,r,nv) v[i]=v[i-1]-1;
+    rep(i,0,nv) s[i]=v[i]*(v[i]+1)/2-1;
+    for(ll p=2; p<=r; p++) {
+        if(s[nv-p]>s[nv-p+1]) {
+        	ll sp=s[nv-p+1],p2=p*p;
 			rep(i,0,nv) {
 				if(v[i]<p2) break;
 				s[i]-=p*(s[ppt(v[i]/p,n,ndr,nv)]-sp);
@@ -24,8 +24,7 @@ ll preprimesum(ll n) {
 }
 void solve(){
 	ll n,k;scanf("%lld%lld",&n,&k);
-	__int128_t one=1,ans=one*(n+4)*(n-1);
-	ans=(ans/2)%k;
+	ll ans=(((__int128_t)n+4)*(n-1)/2)%k;
 	ans+=preprimesum(n+1);
 	ll ans1=(ans-2+k)%k;
 	cout<<ans1<<endl;
