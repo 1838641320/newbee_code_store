02194097e2de21f16b9d32c8ebf85f78a71a2940
1838641320 <60167568+1838641320@users.noreply.github.com>
2021-11-16 15:41:22 +0800

    Update 整数快读模板和快写模板.cpp

--- 21.11/整数快读模板和快写模板.cpp
+++ 21.11/整数快读模板和快写模板.cpp
@@ -1,3 +1,8 @@
+int gc(){
+	const int Q=(1<<23)+1;
+	static char in[Q],*is=in,*it=in;
+	return is==it?it=(is=in)+fread(in,1,Q,stdin),(is==it?EOF:*is++):*is++;
+}
 template<class A>
 void print(A a,char c=' '){
 	static char o1[99]={},*o=o1;
@@ -13,9 +18,10 @@ void print(A a,char c,B...b){
 	print(b...);
 }
 template<class T> void read(T&a){
-	a=0;int f=0,c=getchar();
-	for(;!isdigit(c);c=getchar()) f|=c=='-';
-	for(;isdigit(c);c=getchar()) a=a*10+(c^'0');
+	a=0;int f=0,c=gc();
+	for(;!isdigit(c);c=gc()) f|=c=='-';
+	for(;isdigit(c);c=gc()) a=a*10+(c^'0');
 	if(f) a=-a;
 }
-template<class A,class...B> void read(A&x,B&...y){read(x),read(y...);}
+template<class A,class...B>
+void read(A&x,B&...y){read(x),read(y...);}
