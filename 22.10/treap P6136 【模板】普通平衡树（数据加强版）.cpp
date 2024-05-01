0b7638483065254ee3a61ea5d3e0a1a71ff10586
1838641320 <60167568+1838641320@users.noreply.github.com>
2024-05-01 23:33:04 +0800

    Update treap P6136 【模板】普通平衡树（数据加强版）.cpp

--- 22.10/treap P6136 【模板】普通平衡树（数据加强版）.cpp
+++ 22.10/treap P6136 【模板】普通平衡树（数据加强版）.cpp
@@ -7,7 +7,6 @@ using ll=long long;
 #include<array>
 template<class ty>
 struct treap{
-private:
     mt19937 rd;
     vector<array<int,2>> ch;
     vector<int> sz,pri;
@@ -18,7 +17,12 @@ private:
         return n;
     }
     void update(int u){sz[u]=sz[ch[u][0]]+sz[ch[u][1]]+1;}
-    void down(int u){}
+    void down(int u){
+        // if(ch[u][0]) val[ch[u][0]].sub+=val[u].sub;
+        // if(ch[u][1]) val[ch[u][1]].sub+=val[u].sub;
+        // val[u].val-=val[u].sub;
+        // val[u].sub=0;
+    }
     int merge(int l,int r){//return new root;
         if((ll)l*r==0) return l+r;//null return;
         if(pri[l]<pri[r])
@@ -26,26 +30,35 @@ private:
         else 
             return down(r),ch[r][0]=merge(l,ch[r][0]),update(r),r;
     }
-    void split(ty v,int in,int&l,int&r){//l<=v,r>v
+    void split_by_val(ty v,int in,int&l,int&r){//l<=v,r>v
         if(!in) return void(l=r=0);
         down(in);
-        if(val[in]<=v)
-            l=in,split(v,ch[in][1],ch[in][1],r);
+        if(!(v<val[in])) //v>=val[in]
+            l=in,split_by_val(v,ch[in][1],ch[in][1],r);
         else 
-            r=in,split(v,ch[in][0],l,ch[in][0]);
+            r=in,split_by_val(v,ch[in][0],l,ch[in][0]);
         update(in);
     }
-public:
+    void split_by_rank(int rk,int in,int&l,int&r){//l<=v,r>v
+        if(!in) return void(l=r=0);
+        down(in);
+        if(sz[ch[in][0]]<rk)
+            l=in,split_by_rank(rk-sz[ch[in][0]]-1,ch[in][1],ch[in][1],r);
+        else 
+            r=in,split_by_rank(rk,ch[in][0],l,ch[in][0]);
+        update(in);
+    }
+//----------------------------------------------------------------
     treap(size_t n):rd(time(0)),ch(n+1),sz(n+1),pri(n+1),val(n+1){
         this->n=this->root=0;
     }
     void insert(ty v){
-        split(v,root,L,R);
+        split_by_val(v,root,L,R);
         root=merge(L,merge(newnode(v),R));
     }
     void erase(ty v){
-        split(v-1,root,L,R);
-        split(v,R,M,R);
+        split_by_val(v-1,root,L,R);
+        split_by_val(v,R,M,R);
         M=merge(ch[M][0],ch[M][1]);
         root=merge(L,merge(M,R));
     }
