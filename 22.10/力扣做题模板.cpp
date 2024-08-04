7e77d3395181b2053c5cf40384af6f178acd9bba
1838641320 <60167568+1838641320@users.noreply.github.com>
2024-08-04 18:30:31 +0800

    Update 力扣做题模板.cpp

--- 22.10/力扣做题模板.cpp
+++ 22.10/力扣做题模板.cpp
@@ -3,103 +3,45 @@
 #include<cstring>
 using namespace std;
 #include<queue>
-//---------------------------------------------------------
-using ll=long long;
-
-class Solution {
-public:
-    int minimumDistance(vector<vector<int>>& a) {
-        
-    }
-};
-//---------------------------------------------------
-#include<sstream>
-namespace my_util{
 template<class t>
 ostream& operator<<(ostream&os,const vector<t> &a){
 	for(auto i=0u;i<size(a);i++) 
 		os<<",{"[!i]<<a[i];
 	return os<<"}\0{}"+empty(a)*2;
 }
-template<typename T>
-T convertFromString(const char *&str,int &len) {
-    std::istringstream iss(str);
-    T result;
-    iss >> result;
-    {auto a=iss.tellg();
-    iss.seekg(0, std::ios::end);
-    str+=(len=strlen(str)-(iss.tellg()-a));}
-    return result;
-}
-template<typename T>
-T convertFromString(const char *&str) {
-    int tmp;
-    return convertFromString<T>(str,tmp);
-}
-auto parse(const char *&s,auto getf,auto pushf){
-    auto a=getf;
-    s+=1;
-    for(;s[0]!=']';){
-        pushf(a,s);
-        if(s[0]==',') s++;
-    }
-    s+=1;
-    return a;
-}
-template<typename ty>
-auto fpush1(vector<ty> &a,const char *&s){
-    a.push_back(convertFromString<ty>(s));
-}
-template<typename ty>
-auto fpush2(vector<vector<ty>> &a,const char*&s){
-    a.push_back(parse(s,vector<ty>{},fpush1<ty>));
-}
-auto operator ""_V2(const char* s,size_t ){
-    return parse(s,vector<vector<int>>{},fpush2<int>);
-}
-auto operator ""_V1(const char* s,size_t ){
-    return parse(s,vector<int>{},fpush1<int>);
-}
-}
+//---------------------------------------------------------
+
+class Solution {
+public:
+	vector<int> numberOfAlternatingGroups(vector<int>& c, vector<vector<int>>& q) {
+		
+	}
+};
+//---------------------------------------------------------
+#include<random>
 int main(){
-	ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    freopen("1.txt","r",stdin);
-    // freopen("CON","r",stdin);
-    using namespace my_util;
+	// ios::sync_with_stdio(0);
+	// cin.tie(0);cout.tie(0);
+	// freopen("1.txt","r",stdin);
+	// freopen("CON","r",stdin);
+	mt19937 rd(time(0));
+	auto gen=[&](int l,int r){
+		return rd()%(r-l+1)+l;
+	};
 	Solution s;
-	vector<vector<int>> g,g2;
-	vector<int> a,b;
-    a="[1,2,3,4]"_V1;
-    g="[[5,5],[6,6]]"_V2;
-    vector<string> w={"a","aba","ababa","aa"};
-	g2={{0,2},{1,2},{4,2},{3,4}};
-	g={{3,4,2,1},{4,2,1,1},{2,1,1,0},{3,4,1,0}};
-    g={{2, 1, 1, 1, 1, 2, 2, 1, 0},
-        {0, 0, 1, 2, 0, 0, 0, 2, 2},
-        {2, 0, 2, 2, 1, 2, 1, 0, 0}};
-    a={0,0,0,0};
-	b={2,4,9,5};
-	cout<<s.minimumDistance(g)<<"\n";
+	{
+		
+	}
+	// for(int i=0;i<1e5;i++){
+	//     int n=gen(1,10);
+	//     // n=4e4;
+	// }
 }
-
-
+// -g3 -fsanitize=address,undefined
 
 /*
--o zhong_dao_da.exe
-#include <iostream>
-#include<vector>
-using namespace std;
-int main() {
-    string s;
-    vector<string> t;
-    while(getline(cin,s)){
-        for(char&c:s){
-            if(c=='[') c='{';
-            if(c==']') c='}';
-        }
-        t.push_back(s);
-    }
-    for(auto i:t) cout<<i<<"\n";
-}
+[1,10,9]
+[1,1,3,3]
+
+
 */
