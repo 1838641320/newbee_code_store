fbf972af30c206d6358b781a99a3c77b773746e8
1838641320 <60167568+1838641320@users.noreply.github.com>
2024-10-13 18:12:42 +0800

    Update 力扣做题模板.cpp

--- 22.10/力扣做题模板.cpp
+++ 22.10/力扣做题模板.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <algorithm>
-#include<cstring>
+#include <cstring>
 using namespace std;
 #include<queue>
 template<class t>
@@ -9,29 +9,39 @@ ostream& operator<<(ostream&os,const vector<t> &a){
 		os<<",{"[!i]<<a[i];
 	return os<<"}\0{}"+empty(a)*2;
 }
+// #include<set>
+// #include<map>
 //---------------------------------------------------------
-
 class Solution {
 public:
-	vector<int> numberOfAlternatingGroups(vector<int>& c, vector<vector<int>>& q) {
-		
-	}
+    vector<long long> findXSum(vector<int>& a, int k, int x) {
+        using ll=long long;
+        vector<ll> ans;
+        
+        return ans;
+    }
 };
 //---------------------------------------------------------
 #include<random>
+char sss[1<<22];
 int main(){
 	// ios::sync_with_stdio(0);
 	// cin.tie(0);cout.tie(0);
-	// freopen("1.txt","r",stdin);
+	freopen("2.txt","r",stdin);
 	// freopen("CON","r",stdin);
 	mt19937 rd(time(0));
-	auto gen=[&](int l,int r){
+	[[maybe_unused]] auto gen=[&](int l,int r){
 		return rd()%(r-l+1)+l;
 	};
+	auto print=[](auto v){cout<<v<<"\n";};
 	Solution s;
 	{
-		
+vector<int> a={1,1,2,2,3,4,2,3};
+int b=6;
+int c=2;
+		print(s.findXSum(a,b,c));
 	}
+
 	// for(int i=0;i<1e5;i++){
 	//     int n=gen(1,10);
 	//     // n=4e4;
@@ -40,8 +50,6 @@ int main(){
 // -g3 -fsanitize=address,undefined
 
 /*
-[1,10,9]
-[1,1,3,3]
-
+[[0,1],[0,4],[0,5],[1,7],[2,3],[2,4],[2,5],[3,6],[4,6],[4,7],[6,8],[7,8]]
 
 */
